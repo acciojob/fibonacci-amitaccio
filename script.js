199e5ef137b04d697e989bdeb31ddebe284f8ef7
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-12-01 04:16:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function fibonacci(num) {
-// your code here
+    let a = 0;
+    let b = 1;
+    let temp;
+
+    if (num === 0) {
+        return a;
+    } else if (num === 1) {
+        return b;
+    } else {
+        for (let i = 2; i <= num; i++) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
 }
+
+// Test the function
+const n = 7; // Change this value to test different nth Fibonacci numbers
+console.log(`The ${n}th Fibonacci number is: ${fibonacci(n)}`);
+
 
 module.exports = fibonacci;
